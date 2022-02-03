b7272b706cfb69b45e0594cb03d1edc0b7ebe785
theeraphat claisuban <t00879@g-able.com>
2022-02-03 14:04:19 +0700

    second

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import { AuthProvider } from './pages/Auth'
+import Mainpage from './pages/mainpage'
+import GlobalStyles from'./components/GlobalStyles'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <AuthProvider>
+      <Router>
+      <GlobalStyles />
+        <Switch>
+          <Route exact path="/" component={Mainpage}/>
+        </Switch>
+      </Router>
+    // </AuthProvider>
   );
 }
 
